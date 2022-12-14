3cc456ad7227f41048e4e16de96d1b97734e22f6
bedroesb <bedro@psb.vib-ugent.be>
2022-12-14 14:52:43 +0100

    trying out new structure

--- lib/elixir-toolkit-theme-plugins.rb
+++
@@ -1,5 +0,0 @@
-
-# frozen_string_literal: true
-
-require "elixir-toolkit-theme-plugins/jekyll-tool-table-filter"
-require "elixir-toolkit-theme-plugins/jekyll-tool-tag"
---
+++ lib/jekyll/ett.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'jekyll'
+
+require "ett/jekyll-tool-table-filter"
+require "ett/jekyll-tool-tag"
